6c8aa97aafad742611aec85e18111b26808b6ae4
Kamil Sobieraj <kamil.sobieraj.dev@gmail.com>
2019-06-18 11:49:40 +0200

    Basics: - store created

---
+++ redux-basics.js
@@ -0,0 +1,15 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
+const initialState = {
+  counter: 0
+};
+
+// REDUCER
+const rootReducer = (state = initialState, action) => {
+  return state;
+};
+
+// STORE
+const store = createStore(rootReducer);
+console.log(store.getState());
